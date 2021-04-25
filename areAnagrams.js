1912b0efcc94919a8df82eeef99eb88c4be11b0a
Konstantin Kharlamov <k.kharlamow@gmail.com>
2021-04-26 00:02:55 +0300

    Create areAnagrams.js

---
+++ areAnagrams.js
@@ -0,0 +1,39 @@
+areAnagrams('abdcef', 'abcdef'); // true
+areAnagrams('a', ''); // false
+areAnagrams('aabdcef', 'abcddef'); // false
+areAnagrams('abdcedf', 'abdcdef'); // true
+areAnagrams('abdcef', 'abcdefabcdef'); // false
+areAnagrams('abcdefabcdef', 'abdcef'); // false
+
+function areAnagrams(a, b) {
+    const dictA = toDict(a);
+	const dictB = toDict(b);
+	
+	if (Object.keys(dictA).length !== Object.keys(dictB).length) {
+	    return false;
+	}
+
+	for (letter in dictA) {
+        if (!dictB[letter]) {
+		    return false;
+		}
+		
+		if (dictA[letter] !== dictB[letter]) {
+		    return false;
+		}
+	}
+	
+	return true;
+}
+
+function toDict(str) {
+    const dict = Object.create(null);
+	const arr = str.split('');
+
+	for (letter of arr) {
+	    dict[letter] = dict[letter] ? ++dict[letter] : 1;
+		
+	}
+
+	return dict;
+}
