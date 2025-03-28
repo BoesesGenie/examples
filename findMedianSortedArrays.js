aa1df1b4f93438d3b76acfe83b9cf4fa64c38373
Konstantin Kharlamov <k.kharlamow@gmail.com>
2025-03-29 01:54:06 +0500

    findMedianSortedArrays

--- findMedianSortedArrays.js
+++ findMedianSortedArrays.js
@@ -1,71 +1,86 @@
 /**
- * @param {number[]} nums1in
- * @param {number[]} nums2in
+ * @param {number[]} nums1
+ * @param {number[]} nums2
  * @return {number}
  */
-const findMedianSortedArrays = function(nums1in, nums2in) {
-  const middle = [];
-  const middleNeg = [];
-  let i = nums1in.length, j = 0;
-  let nums1 = nums1in;
-  let nums2 = nums2in;
+const findMedianSortedArrays = function(nums1, nums2) {
+  if (!nums1.length && !nums2.length) {
+    return 0;
+  }
 
-  if (nums1.length && nums2.length) {
-    if (nums2[0] < nums1[0]) {
-      const tmp = nums2;
+  if (!nums1.length) {
+    const middleIdx = Math.floor(nums2.length / 2);
 
-      nums2 = nums1;
-      nums1 = tmp;
+    return nums2.length % 2 === 0 ?
+      (nums2[middleIdx - 1] + nums2[middleIdx]) / 2 :
+      nums2[middleIdx];
+  }
+
+  if (!nums2.length) {
+    const middleIdx = Math.floor(nums1.length / 2);
+
+    return nums1.length % 2 === 0 ?
+      (nums1[middleIdx - 1] + nums1[middleIdx]) / 2 :
+      nums1[middleIdx];
+  }
+
+  let nums1ordered = nums1;
+  let nums2ordered = nums2;
+
+  if (nums2ordered[0] < nums1ordered[0]) {
+    const tmp = nums2ordered;
+
+    nums2ordered = nums1ordered;
+    nums1ordered = tmp;
+  }
+
+  if (nums1ordered[nums1ordered.length - 1] <= nums2ordered[0]) {
+    const merged = nums1ordered.concat(nums2ordered);
+    const middleIdx = Math.floor(merged.length / 2);
+
+    return merged.length % 2 === 0 ?
+      (merged[middleIdx - 1] + merged[middleIdx]) / 2 :
+      merged[middleIdx];
+  }
+
+  const mergedLength = nums1ordered.length + nums2ordered.length;
+  const merged = [];
+
+  for (let i = 0, j = 0; i + j < mergedLength;) {
+    if (nums1ordered[i] !== undefined && nums1ordered[i] < nums2ordered[0]) {
+      merged.push(nums1ordered[i]);
+
+      i++;
+
+      continue;
     }
 
-    for (i = nums1.length - 1, j = 0; i >= 0 || j < nums2.length; i--, j++) {
-      if (i >= 0 && nums1[i] >= 0) {
-        Array.isArray(middle[nums1[i]]) ?
-          middle[nums1[i]].push(nums1[i]) :
-          middle[nums1[i]] = [nums1[i]];
-      } else if (i >= 0) {
-        const idx = -nums1[i];
+    if (nums1ordered[i] !== undefined && nums2ordered[j] !== undefined) {
+      merged.push(Math.min(nums1ordered[i], nums2ordered[j]));
 
-        Array.isArray(middleNeg[idx]) ?
-          middleNeg[idx].push(nums1[i]) :
-          middleNeg[idx] = [nums1[i]];
-      }
+      nums1ordered[i] <= nums2ordered[j] ? i++ : j++;
 
-      if (j < nums2.length && nums2[j] >= 0) {
-        Array.isArray(middle[nums2[j]]) ?
-          middle[nums2[j]].push(nums2[j]) :
-          middle[nums2[j]] = [nums2[j]];
-      } else if (j < nums2.length) {
-        const idx = -nums2[j];
+      continue;
+    }
 
-        Array.isArray(middleNeg[idx]) ?
-          middleNeg[idx].push(nums2[j]) :
-          middleNeg[idx] = [nums2[j]];
-      }
+    if (nums1ordered[i] !== undefined) {
+      merged.push(nums1ordered[i]);
 
-      if ((!middleNeg.length || middleNeg[0] < nums2[j < nums2.length ? j : nums2.length - 1]) &&
-        (!middle.length || middle[middle.length - 1] < nums2[j < nums2.length ? j : nums2.length - 1])
-      ) {
-        i--;
-        j++;
-        break;
-      }
+      i++;
+
+      continue;
+    }
+
+    if (nums2ordered[j] !== undefined) {
+      merged.push(nums2ordered[j]);
+
+      j++;
     }
   }
 
-  middleNeg.reverse();
+  const middleIdx = Math.floor(mergedLength / 2);
 
-  i = i >= 0 ? i + 1 : 0;
-  j = j < nums2.length ? j : nums2.length;
-
-  const merged = nums1
-    .slice(0, i)
-    .concat(middleNeg.filter((item) => Array.isArray(item)).reduce((acc, item) => acc.concat(item), []))
-    .concat(middle.filter((item) => Array.isArray(item)).reduce((acc, item) => acc.concat(item), []))
-    .concat(nums2.slice(j));
-  const middleIdx = parseInt(merged.length / 2);
-
-  return merged.length % 2 === 0 ?
+  return mergedLength % 2 === 0 ?
     (merged[middleIdx - 1] + merged[middleIdx]) / 2 :
     merged[middleIdx];
 };
