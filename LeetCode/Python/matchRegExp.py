bb09998a27d3f27c601e2d9aef48ef778bc0c4c5
Konstantin Kharlamov <k.kharlamow@gmail.com>
2025-04-05 01:02:29 +0500

    with bool matrix

--- LeetCode/Python/matchRegExp.py
+++ LeetCode/Python/matchRegExp.py
@@ -1,145 +1,40 @@
+# https://leetcode.com/problems/regular-expression-matching/
+
 class Solution:
-    completed = False
-    status = False
-    s_left = 0
-    s_right = 0
-    p_index = 0
-    s = ''
-    p = ''
-    state = [(0, 0, 0)]
-
-    def initial_values(self):
-        self.completed = False
-        self.status = False
-        self.s_left = 0
-        self.s_right = 0
-        self.p_index = 0
-        self.s = ''
-        self.p = ''
-        self.state = [(0, 0)]
-
-    def state_back(self):
-        self.state.pop()
-        self.s_left, self.s_right = self.state[-1]
-
-    def update_state(self):
-        self.state.append((self.s_left, self.s_right))
-
-        if self.s_right == len(self.s) and self.p_index == len(self.p):
-            self.completed = True
-            self.status = True
-
-        elif self.s_left == len(self.s) and self.p_index == len(self.p):
-            self.completed = True
-            self.status = True
-
-        elif self.p_index == len(self.p) and self.s_right < len(self.s):
-            self.completed = True
-            self.status = False
-
-        elif self.p_index == len(self.p) and self.s_left > len(self.s):
-            self.completed = True
-            self.status = False
-
-    def handle_mult(self):
-        self.state_back()
-        check = self.p[self.p_index - 1]
-
-        while self.s_right < len(self.s):
-            if check == '.' or check == self.s[self.s_right]:
-                self.s_right += 1
-            else:
-                break
-
-        self.p_index += 1
-
-    def handle_dot(self):
-        if self.s_left >= len(self.s):
-            self.s_left += 1
-            self.s_right = max(self.s_right, self.s_left)
-        elif self.s_left == self.s_right:
-            self.s_left += 1
-            self.s_right += 1
-        else:
-            self.s_left = self.s_right
-
-            self.s_left += 1
-            self.s_right = max(self.s_right, self.s_left)
-
-        self.p_index += 1
-
-    def handle_letter(self):
-        if self.s_left >= len(self.s):
-            self.s_left += 1
-            self.s_right = max(self.s_right, self.s_left)
-        elif self.s_left == self.s_right and self.p[self.p_index] == self.s[self.s_left]:
-            self.s_left += 1
-            self.s_right += 1
-        elif self.s_left < self.s_right:
-            while self.s_left < self.s_right and self.p[self.p_index] != self.s[self.s_left]:
-                self.s_left += 1
-
-            self.s_left += 1
-            self.s_right = max(self.s_right, self.s_left)
-
-        self.p_index += 1
-
     def match_reg_exp(self, s, p):
-        self.initial_values()
-        self.s = s
-        self.p = p
+        m = len(s)
+        n = len(p)
+        M = [[False for _ in range(n + 1)] for _ in range(m + 1)]
 
-        while not self.completed:
-            if p[self.p_index] == '*':
-                self.handle_mult()
-                self.update_state()
-                continue
+        M[0][0] = True
 
-            if p[self.p_index] == '.':
-                self.handle_dot()
-                self.update_state()
-                continue
+        i = 1
+        while i < n + 1:
+            if p[i - 1] == '*':
+                M[0][i] = M[0][i - 2]
 
-            self.handle_letter()
-            self.update_state()
+            i += 1
 
-        return self.status
+        i = 1
 
-solution = Solution()
+        while i < m + 1:
+            j = 1
+            while j < n + 1:
+                current_s = s[i - 1]
+                current_p = p[j - 1]
 
-print(solution.match_reg_exp('abc', 'abc') == True)
-print(solution.match_reg_exp('abc', '.*') == True)
-print(solution.match_reg_exp('abc', '...') == True)
-print(solution.match_reg_exp('abc', '..') == False)
-print(solution.match_reg_exp('abc', '....') == False)
-print(solution.match_reg_exp('abc', 'abd') == False)
-print(solution.match_reg_exp('abc', 'cbc') == False)
-print(solution.match_reg_exp('abc', '.bc') == True)
-print(solution.match_reg_exp('abc', 'a.c') == True)
-print(solution.match_reg_exp('abc', 'ab.') == True)
-print(solution.match_reg_exp('abc', '..c') == True)
-print(solution.match_reg_exp('abc', '.*c') == True)
-print(solution.match_reg_exp('asdfasdfabc', '.*abc') == True)
-print(solution.match_reg_exp('asdfasdfabc', '........abc') == True)
-print(solution.match_reg_exp('asdfasdfabc', '.......abc') == False)
-print(solution.match_reg_exp('asdfamdfabc', 'asd.*abc') == True)
-print(solution.match_reg_exp('asdfasdfabc', 'asd.*abc') == True)
-print(solution.match_reg_exp('asdfasdfabc', 'asd.*') == True)
-print(solution.match_reg_exp('asm', '.*asd') == False)
-print(solution.match_reg_exp('aa', 'a') == False)
-print(solution.match_reg_exp('mississippi', 'mis*is*ip*.') == True)
-print(solution.match_reg_exp('abcd', 'd*') == False)
-print(solution.match_reg_exp('aaa', 'aaaa') == False)
-print(solution.match_reg_exp('a', 'ab*a') == False)
-print(solution.match_reg_exp('asd', 'asd.*') == True)
-print(solution.match_reg_exp('abb', 'bbb*') == False)
-print(solution.match_reg_exp('aa', 'a*') == True)
-print(solution.match_reg_exp('aa', '.*..a*') == False)
-print(solution.match_reg_exp('aaa', 'ab*a*c*a') == True)
-print(solution.match_reg_exp('aab', 'c*a*b') == True)
-print(solution.match_reg_exp('a', 'c*a') == True)
-print(solution.match_reg_exp('aba', '.*.*') == True)
-print(solution.match_reg_exp('bab', '..*') == True)
-print(solution.match_reg_exp('baba', 'b*.*') == True)
-print(solution.match_reg_exp('asd', '.*asd') == True)
-print(solution.match_reg_exp('aaca', 'ab*a*c*a') == True)
+                if current_s == current_p or current_p == '.':
+                    M[i][j] = M[i - 1][j - 1]
+
+                elif current_p == '*':
+                    prev = p[j - 2]
+                    if prev != '.' and prev != current_s:
+                        M[i][j] = M[i][j - 2]
+                    else:
+                        M[i][j] = M[i - 1][j] or M[i][j - 1] or M[i][j - 2]
+
+                j += 1
+
+            i += 1
+
+        return M[m][n]
