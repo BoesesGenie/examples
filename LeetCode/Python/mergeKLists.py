c15535d8e839ab3d64acaa389f18dc750f62409b
Konstantin Kharlamov <k.kharlamow@gmail.com>
2025-04-22 19:26:45 +0500

    mergeKLists optimization

--- LeetCode/Python/mergeKLists.py
+++ LeetCode/Python/mergeKLists.py
@@ -6,41 +6,33 @@
 #         self.val = val
 #         self.next = next
 class Solution(object):
-    def mergeTwoLists(self, list1, list2):
-        result = ListNode()
-        first = list1
-        second = list2
-        current = result
-
-        while first or second:
-            if first and not second:
-                current.next = first
-                first = first.next
-            elif second and not first:
-                current.next = second
-                second = second.next
-            elif first.val <= second.val:
-                current.next = first
-                first = first.next
-            else:
-                current.next = second
-                second = second.next
-
-            current = current.next
-
-        return result.next
-
     def mergeKLists(self, lists):
+        lists = filter(lambda i: not i is None, lists)
+
         if len(lists) == 0:
             return None
 
         if len(lists) == 1:
             return lists[0]
 
-        result = lists[0]
+        result = ListNode()
+        current = result
 
-        for i in range(1, len(lists)):
-            result = self.mergeTwoLists(result, lists[i])
+        while len(lists):
+            min_index = 0
+            min_value = lists[0].val
 
+            for i in range(1, len(lists)):
+                if min_value >= lists[i].val:
+                    min_index = i
+                    min_value = lists[i].val
 
-        return result
+            current.next = lists[min_index]
+            current = current.next
+
+            if lists[min_index].next is None:
+                lists.pop(min_index)
+            else:
+                lists[min_index] = lists[min_index].next
+
+        return result.next
